b19b414e8069eba2af80ca66adfc9f272ac6a62f
JepCast <ycastillop2@miumg.edu.gt>
2025-01-26 11:40:19 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {Footer, Blog, Posibility, Features, WhatGPT3, Header} from './containers';
+import {CTA, Brand, Navbar} from './components'
+function App() {
+  return (
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Posibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
